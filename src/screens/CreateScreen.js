a311ed944afabd537f70bfcf4b37a9fdbce3cd03
Prodigy00 <simplygiddy@gmail.com>
2019-12-25 21:30:10 +0100

    wired up create Screen

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Context } from "../context/BlogContext";
+
+const CreateScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Create</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
