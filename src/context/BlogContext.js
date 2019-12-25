38afe37b3841e7c1a42067595ac72b6dca7436cc
Prodigy00 <simplygiddy@gmail.com>
2019-12-25 19:57:43 +0100

    added blog post icon for adding blog posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,15 +1,17 @@
 import createDataContext from "./createDataContext";
 
+const ADD_BLOG_POST = "add_blog_post";
+
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case "add_blog_post":
+    case ADD_BLOG_POST:
       return [...state, { title: `Blog Post #${state.length + 1}` }];
     default:
       return state;
   }
 };
 
-const addBlogPost = dispatch => () => dispatch({ type: "add_blog_post" });
+const addBlogPost = dispatch => () => dispatch({ type: ADD_BLOG_POST });
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,25 +1,64 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity
+} from "react-native";
 import { Context } from "../context/BlogContext";
-
+import { Feather, AntDesign } from "@expo/vector-icons";
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Text>Index Screen</Text>
-      <Button title="Add Post" onPress={addBlogPost} />
+      {/* <Button title="Add Post" onPress={addBlogPost} /> */}
+      <TouchableOpacity style={styles.buttonStyle} onPress={addBlogPost}>
+        {/* <Text style={styles.buttonTitle}>ADD POST</Text> */}
+        <AntDesign style={styles.icon} name="addfile" />
+      </TouchableOpacity>
       <FlatList
         data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather style={styles.icon} name="trash-2" />
+            </View>
+          );
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 8,
+    borderBottomWidth: 1,
+    borderColor: "gray"
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  },
+  buttonStyle: {
+    alignItems: "center",
+    marginVertical: 10
+  },
+  buttonTitle: {
+    fontSize: 18,
+    color: "blue",
+    textDecorationLine: "underline"
+  }
+});
 
 export default IndexScreen;
