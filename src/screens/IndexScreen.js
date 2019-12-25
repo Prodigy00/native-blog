67cbbc8c79aba2077702ecb425cd96b666193a44
Prodigy00 <simplygiddy@gmail.com>
2019-12-25 18:35:46 +0100

    added callback to blog context

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,21 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
+
   return (
     <View>
       <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
